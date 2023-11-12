b9043957dfecba6a3a7228402fe3ed8cb93eecaa
Руслан Давлетов <lofranlof@gmail.com>
2023-11-12 20:52:32 +0300

    web lab#4 i guess

---
+++ JS/timeToLoad.js
@@ -0,0 +1,27 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    let startTime = performance.now();
+
+    window.addEventListener('load', function() {
+        var loadTime = performance.now() - startTime;
+        console.log('Время загрузки страницы: ' + loadTime + ' мс');
+
+        var footer = document.createElement('footer');
+        footer.innerHTML = 'Время загрузки страницы: ' + loadTime.toFixed(3) + ' мс';
+        document.body.appendChild(footer);
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var menuItems = document.querySelectorAll('a');
+    var currentPage = document.location.pathname;
+
+    menuItems.forEach(function(item) {
+        var itemPath = item.getAttribute('href');
+
+        if (currentPage === itemPath) {
+            item.classList.add('active');
+        }
+    });
+});
+
